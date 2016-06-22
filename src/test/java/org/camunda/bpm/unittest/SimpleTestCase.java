60d7baef9ab0b946a07fe5f07cf3e8a0b27c6a5e
Christoph Keimel <c.keimel@emsw.de>
2016-06-22 10:04:35 +0200

    Test of TaskService to find specific task in multi instance body

--- src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
+++ src/test/java/org/camunda/bpm/unittest/SimpleTestCase.java
@@ -12,12 +12,21 @@
  */
 package org.camunda.bpm.unittest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineAssertions.assertThat;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.processInstanceQuery;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.runtimeService;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.taskQuery;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.taskService;
+import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.withVariables;
+
+import java.util.Arrays;
+import java.util.List;
+
 import org.camunda.bpm.engine.runtime.ProcessInstance;
+import org.camunda.bpm.engine.task.Task;
 import org.camunda.bpm.engine.test.Deployment;
 import org.camunda.bpm.engine.test.ProcessEngineRule;
-
-import static org.camunda.bpm.engine.test.assertions.ProcessEngineTests.*;
-
 import org.junit.Rule;
 import org.junit.Test;
 
@@ -27,25 +36,37 @@ import org.junit.Test;
  */
 public class SimpleTestCase {
 
-  @Rule
-  public ProcessEngineRule rule = new ProcessEngineRule();
+	@Rule
+	public ProcessEngineRule rule = new ProcessEngineRule();
 
-  @Test
-  @Deployment(resources = {"testProcess.bpmn"})
-  public void shouldExecuteProcess() {
-    // Given we create a new process instance
-    ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess");
-    // Then it should be active
-    assertThat(processInstance).isActive();
-    // And it should be the only instance
-    assertThat(processInstanceQuery().count()).isEqualTo(1);
-    // And there should exist just a single task within that process instance
-    assertThat(task(processInstance)).isNotNull();
+	@Test
+	@Deployment(resources = { "testProcess.bpmn" })
+	public void shouldExecuteProcess() {
 
-    // When we complete that task
-    complete(task(processInstance));
-    // Then the process instance should be ended
-    assertThat(processInstance).isEnded();
-  }
+		Long searchId = 42l;
+		List<Long> someIdList = Arrays.asList(new Long[] { 10l, 28l, searchId });
+		
+		// Given we create a new process instance
+		ProcessInstance processInstance = runtimeService().startProcessInstanceByKey("testProcess",
+				withVariables("someIdList", someIdList));
+		// Then it should be active
+		assertThat(processInstance).isActive();
+		// And it should be the only instance
+		assertThat(processInstanceQuery().count()).isEqualTo(1);
+
+		List<Task> list = taskQuery().list();
+		assertThat(list).hasSize(someIdList.size());
+		
+		List<Task> searchResult = taskService().createTaskQuery()
+				.processVariableValueEquals("someId", searchId)
+				.list();
+		System.out.println(searchResult);
+		assertThat(searchResult).hasSize(1);
+		
+		// When we complete that task
+		// complete(task(processInstance));
+		// Then the process instance should be ended
+		// assertThat(processInstance).isEnded();
+	}
 
 }
